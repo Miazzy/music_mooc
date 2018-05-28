10b85c2b790b9c6f2f37dcd576081ce577713b7a
MicroIMOOC <1434246346@qq.com>
2018-05-28 22:11:38 +0800

    滚动固定标题实现

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -19,14 +19,24 @@
         </li>
       </ul>
     </div>
+
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <div class="fixed-title">{{fixedTitle}}</div>
+    </div>
+
+    <div class="loading-container" v-show="!data.length">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script>
-import Scroll from "base/scroll/scroll";
+import Scroll from "base/scroll/scroll"
+import Loading from 'base/loading/loading'
 import { getData } from "common/js/dom"
 
 const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
 
 export default {
   props: {
@@ -36,12 +46,14 @@ export default {
     }
   },
   components: {
-    Scroll
+    Scroll,
+    Loading
   },
   data() {
     return {
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: -1
     }
   },
   computed: {
@@ -49,6 +61,12 @@ export default {
       return this.data.map((group) => {
         return group.title.substr(0, 1)
       })
+    },
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   created() {
@@ -116,10 +134,19 @@ export default {
         let height2 = listHeight[i + 1]
         if (-newY >= height1 && -newY < height2) {
           this.currentIndex = i
+          this.diff = height2 + newY
           return
         }
       }
       this.currentIndex = listHeight.length - 2
+    },
+    diff(newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
     }
   }
 };
