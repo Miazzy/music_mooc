b674472b9907d1233c0bf1f962984c70ae7f2b2e
MicroIMOOC <1434246346@qq.com>
2018-06-01 17:37:01 +0800

    歌手详情数据抓取

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -7,6 +7,8 @@
 <script>
 import MusicList from 'components/music-list/music-list'
 import { mapGetters } from 'vuex'
+import { getSingerDetail } from 'api/singer'
+import { ERR_OK } from 'api/config'
 
 export default {
   components: {
@@ -18,7 +20,20 @@ export default {
     ])
   },
   created() {
-    console.log(this.singer)
+    this._getDetail()
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+        }
+      })
+    }
   }
 }
 </script>
