830e1a71dfc1f119cf02a217ea6b979944d47307
MicroIMOOC <1434246346@qq.com>
2018-08-17 15:36:24 +0800

    初始化歌手详情页布局

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <music-list/>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"/>
   </transition>
 </template>
 
@@ -16,6 +16,12 @@
       MusicList
     },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
@@ -37,7 +43,6 @@
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
             this.songs = this._normalizeSongs(res.data.list)
-            console.log(this.songs)
           }
         })
       },
