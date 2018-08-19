2a10a44beba48a833bf2a02cdc843cc9daf6aa2b
MicroIMOOC <1434246346@qq.com>
2018-08-19 20:47:50 +0800

    播放器初始化Vuex数据

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="song-list">
-    <li class="item" v-for="(song,index) in songs" :key="index">
+    <li class="item" v-for="(song,index) in songs" :key="index" @click="selectItem(song, index)">
       <div class="content">
         <h2 class="name">{{song.name}}</h2>
         <p class="desc">{{getDesc(song)}}</p>
@@ -10,59 +10,62 @@
 </template>
 
 <script>
-export default {
-  props: {
-    songs: {
-      type: Array,
-      default: []
-    }
-  },
-  methods: {
-    getDesc(song) {
-      return `${song.singer}·${song.album}`
+  export default {
+    props: {
+      songs: {
+        type: Array,
+        default: []
+      }
+    },
+    methods: {
+      getDesc(song) {
+        return `${song.singer}·${song.album}`
+      },
+      selectItem(song, index) {
+        this.$emit('select', song, index)
+      }
     }
   }
-}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import '~common/stylus/variable'
-@import '~common/stylus/mixin'
-.song-list
-  .item
-    display flex
-    align-items center
-    box-sizing border-box
-    height 64px
-    font-size $font-size-medium
-    .rank
-      flex 0 0 25px
-      width 25px
-      margin-right 30px
-      text-align center
-      .icon
-        display inline-block
+  @import '~common/stylus/variable'
+  @import '~common/stylus/mixin'
+  .song-list
+    .item
+      display flex
+      align-items center
+      box-sizing border-box
+      height 64px
+      font-size $font-size-medium
+      .rank
+        flex 0 0 25px
         width 25px
-        height 24px
-        background-size 25px 24px
-        &.icon0
-          bg-image('first')
-        &.icon1
-          bg-image('second')
-        &.icon2
-          bg-image('third')
-      .text
-        color $color-theme
-        font-size $font-size-large
-    .content
-      flex 1
-      line-height 20px
-      overflow hidden
-      .name
-        no-wrap()
-        color $color-text
-      .desc
-        no-wrap()
-        margin-top 4px
-        color $color-text-d
+        margin-right 30px
+        text-align center
+        .icon
+          display inline-block
+          width 25px
+          height 24px
+          background-size 25px 24px
+          &.icon0
+            bg-image('first')
+          &.icon1
+            bg-image('second')
+          &.icon2
+            bg-image('third')
+        .text
+          color $color-theme
+          font-size $font-size-large
+      .content
+        flex 1
+        line-height 20px
+        overflow hidden
+        .name
+          no-wrap()
+          color $color-text
+        .desc
+          no-wrap()
+          margin-top 4px
+          color $color-text-d
 </style>
---
+++ src/common/js/config.js
@@ -0,0 +1,5 @@
+export const playMode = {
+  sequence: 1,
+  loop: 2,
+  random: 3
+}
