d81e47f6a7a38e056a4125f9e538f6691375e455
MicroIMOOC <1434246346@qq.com>
2018-10-01 13:34:46 +0800

    歌单详情页

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,152 +1,179 @@
 <template>
-	<div class="recommend">
-		<scroll class="recommend-content" ref="scroll" :data="discList">
-			<div>
-				<div class="slider-wrapper" v-if="recommends.length">
-					<div class="slider-content">
-						<slider ref="slider">
-							<div v-for="(item,index) in recommends" :key="index">
-								<a :href="item.linkUrl">
-									<img :src="item.picUrl" @load="loadImage">
-								</a>
-							</div>
-						</slider>
-					</div>
-				</div>
+  <div class="recommend" ref="recommend">
+    <scroll class="recommend-content" ref="scroll" :data="discList">
+      <div>
+        <div class="slider-wrapper" v-if="recommends.length">
+          <div class="slider-content">
+            <slider ref="slider">
+              <div v-for="(item,index) in recommends" :key="index">
+                <a :href="item.linkUrl">
+                  <img :src="item.picUrl" @load="loadImage">
+                </a>
+              </div>
+            </slider>
+          </div>
+        </div>
 
-				<div class="recommend-list">
-					<h1 class="list-title">热门歌单推荐</h1>
-					<ul>
-						<li class="item" v-for="(item,index) in discList" :key="index">
-							<div class="icon">
-								<img width="60" height="60" v-lazy="item.imgurl" alt="">
-							</div>
-							<div class="text">
-								<h2 class="name" v-html="item.creator.name"></h2>
-								<p class="desc" v-html="item.dissname"></p>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li class="item" v-for="(item,index) in discList" :key="index" @click="selectItem(item)">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.imgurl" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-			<div class="loading-container" v-show="!discList.length">
-				<loading />
-			</div>
-		</scroll>
-		<router-view></router-view>
-	</div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading/>
+      </div>
+    </scroll>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import { getRecommend, getDiscList } from "api/recommend";
-import { ERR_OK } from "api/config";
-import Slider from "base/slider/slider";
-import Scroll from "base/scroll/scroll";
-import Loading from "base/loading/loading";
+  import {getRecommend, getDiscList} from "api/recommend";
+  import {ERR_OK} from "api/config";
+  import Slider from "base/slider/slider";
+  import Scroll from "base/scroll/scroll";
+  import Loading from "base/loading/loading";
+  import {playlistMixin} from 'common/js/mixin'
+  import {mapMutations} from 'vuex'
 
-export default {
-	data() {
-		return {
-			recommends: [],
-			discList: []
-		};
-	},
+  export default {
+    mixins: [playlistMixin],
+    data() {
+      return {
+        recommends: [],
+        discList: []
+      };
+    },
 
-	components: {
-		Slider,
-		Scroll,
-		Loading
-	},
+    components: {
+      Slider,
+      Scroll,
+      Loading
+    },
 
-	created() {
-		this._getRecommend();
-		this._getDiscList();
-	},
+    created() {
+      this._getRecommend();
+      this._getDiscList();
+    },
 
-	methods: {
-		_getRecommend() {
-			getRecommend().then(res => {
-				if (res.code === ERR_OK) {
-					this.recommends = res.data.slider;
-				}
-			});
-		},
+    activated() {
+      setTimeout(() => {
+        this.$refs.slider && this.$refs.slider.refresh()
+      }, 20)
+    },
 
-		_getDiscList() {
-			getDiscList().then(res => {
-				if (res.code === ERR_OK) {
-					this.discList = res.data.list;
-				}
-			});
-		},
+    methods: {
+      _getRecommend() {
+        getRecommend().then(res => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider;
+          }
+        });
+      },
 
-		loadImage() {
-			if (!this.checkloaded) {
-				this.checkloaded = true;
-				setTimeout(() => {
-					this.$refs.scroll.refresh();
-				}, 20);
-			}
-		}
-	}
-};
+      _getDiscList() {
+        getDiscList().then(res => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list;
+          }
+        });
+      },
+
+      loadImage() {
+        if (!this.checkloaded) {
+          this.checkloaded = true;
+          setTimeout(() => {
+            this.$refs.scroll.refresh();
+          }, 20);
+        }
+      },
+
+      selectItem(item) {
+        this.$router.push({
+          path: `/recommend/${item.dissid}`
+        })
+        this.setDisc(item)
+      },
+
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+
+        this.$refs.recommend.style.bottom = bottom
+        this.$refs.scroll.refresh()
+      },
+
+      ...mapMutations({
+        setDisc: 'SET_DISC'
+      })
+    }
+  };
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import '~common/stylus/variable'
-.recommend
-	position fixed
-	width 100%
-	top 88px
-	bottom 0
-	.recommend-content
-		height 100%
-		overflow hidden
-		.slider-wrapper
-			position relative
-			width 100%
-			height 0
-			padding-top 40%
-			overflow hidden
-			.slider-content
-				position absolute
-				top 0
-				left 0
-				width 100%
-				height 100%
-		.recommend-list
-			.list-title
-				height 65px
-				line-height 65px
-				text-align center
-				font-size $font-size-medium
-				color $color-theme
-			.item
-				display flex
-				box-sizing border-box
-				align-items center
-				padding 0 20px 20px 20px
-				.icon
-					flex 0 0 60px
-					width 60px
-					padding-right 20px
-				.text
-					display flex
-					flex-direction column
-					justify-content center
-					flex 1
-					line-height 20px
-					overflow hidden
-					font-size $font-size-medium
-					.name
-						margin-bottom 10px
-						color $color-text
-					.desc
-						color $color-text-d
-		.loading-container
-			position absolute
-			width 100%
-			top 50%
-			transform translateY(-50%)
+  @import '~common/stylus/variable'
+  .recommend
+    position fixed
+    width 100%
+    top 88px
+    bottom 0
+    .recommend-content
+      height 100%
+      overflow hidden
+      .slider-wrapper
+        position relative
+        width 100%
+        height 0
+        padding-top 40%
+        overflow hidden
+        .slider-content
+          position absolute
+          top 0
+          left 0
+          width 100%
+          height 100%
+      .recommend-list
+        .list-title
+          height 65px
+          line-height 65px
+          text-align center
+          font-size $font-size-medium
+          color $color-theme
+        .item
+          display flex
+          box-sizing border-box
+          align-items center
+          padding 0 20px 20px 20px
+          .icon
+            flex 0 0 60px
+            width 60px
+            padding-right 20px
+          .text
+            display flex
+            flex-direction column
+            justify-content center
+            flex 1
+            line-height 20px
+            overflow hidden
+            font-size $font-size-medium
+            .name
+              margin-bottom 10px
+              color $color-text
+            .desc
+              color $color-text-d
+      .loading-container
+        position absolute
+        width 100%
+        top 50%
+        transform translateY(-50%)
 </style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,110 +1,117 @@
 <template>
-  <div class="singer">
-    <list-view :data="singers" @select="selectSinger" />
-    <router-view />
+  <div class="singer" ref="singer">
+    <list-view :data="singers" @select="selectSinger" ref="list"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import { getSingerList } from "api/singer";
-import { ERR_OK } from "api/config";
-import Singer from "common/js/singer";
-import ListView from "base/listview/listview";
-import { mapMutations } from 'vuex';
+  import {getSingerList} from "api/singer"
+  import {ERR_OK} from "api/config"
+  import Singer from "common/js/singer"
+  import ListView from "base/listview/listview"
+  import {playlistMixin} from 'common/js/mixin'
+  import {mapMutations} from 'vuex'
 
-const HOT_NAME = "热门";
-const HOT_SINGER_LEN = 10;
+  const HOT_NAME = "热门";
+  const HOT_SINGER_LEN = 10;
 
-export default {
-  data() {
-    return {
-      singers: []
-    };
-  },
-
-  components: {
-    ListView
-  },
-  created() {
-    this._getSingerList();
-  },
-
-  methods: {
-    _getSingerList() {
-      getSingerList().then(res => {
-        if (res.code === ERR_OK) {
-          this.singers = this._normalizeSinger(res.data.list);
-        }
-      });
+  export default {
+    mixins: [playlistMixin],
+    data() {
+      return {
+        singers: []
+      };
     },
 
-    _normalizeSinger(list) {
-      let map = {
-        hot: {
-          title: HOT_NAME,
-          items: []
-        }
-      };
-      list.forEach((item, index) => {
-        if (index < HOT_SINGER_LEN) {
-          map.hot.items.push(
+    components: {
+      ListView
+    },
+    created() {
+      this._getSingerList();
+    },
+
+    methods: {
+      _getSingerList() {
+        getSingerList().then(res => {
+          if (res.code === ERR_OK) {
+            this.singers = this._normalizeSinger(res.data.list);
+          }
+        });
+      },
+
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        };
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(
+              new Singer({
+                name: item.Fsinger_name,
+                id: item.Fsinger_mid
+              })
+            );
+          }
+          const key = item.Findex;
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            };
+          }
+          map[key].items.push(
             new Singer({
               name: item.Fsinger_name,
               id: item.Fsinger_mid
             })
           );
-        }
-        const key = item.Findex;
-        if (!map[key]) {
-          map[key] = {
-            title: key,
-            items: []
-          };
-        }
-        map[key].items.push(
-          new Singer({
-            name: item.Fsinger_name,
-            id: item.Fsinger_mid
-          })
-        );
-      });
+        });
 
-      let ret = [];
-      let hot = [];
-      for (let key in map) {
-        let val = map[key];
-        if (val.title.match(/[a-zA-Z]/)) {
-          ret.push(val);
-        } else if (val.title === HOT_NAME) {
-          hot.push(val);
+        let ret = [];
+        let hot = [];
+        for (let key in map) {
+          let val = map[key];
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val);
+          } else if (val.title === HOT_NAME) {
+            hot.push(val);
+          }
         }
-      }
-      ret.sort((a, b) => {
-        return a.title.charCodeAt(0) - b.title.charCodeAt(0);
-      });
-      return hot.concat(ret);
-    },
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+        });
+        return hot.concat(ret);
+      },
 
-    selectSinger(singer) {
-      this.$router.push({
-        path: `/singer/${singer.id}`
+      selectSinger(singer) {
+        this.$router.push({
+          path: `/singer/${singer.id}`
+        })
+        this.setSinger(singer)
+      },
+
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.singer.style.bottom = bottom
+      },
+
+      ...mapMutations({
+        setSinger: 'SET_SINGER'
       })
-      this.setSinger(singer)
-    },
-
-    ...mapMutations({
-      setSinger: 'SET_SINGER'
-    })
-  }
-};
+    }
+  };
 </script>
 
 
 <style scoped>
-.singer {
-  position: fixed;
-  top: 88px;
-  bottom: 0;
-  width: 100%;
-}
+  .singer {
+    position: fixed;
+    top: 88px;
+    bottom: 0;
+    width: 100%;
+  }
 </style>
