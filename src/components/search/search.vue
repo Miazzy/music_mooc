b84cd5d95d2a117bbd90b57798ffefc7d85a4d05
MicroIMOOC <1434246346@qq.com>
2018-12-01 17:30:21 +0800

    '歌手/歌曲搜索功能实现及优化'

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -14,12 +14,14 @@
               </li>
             </ul>
           </div>
-          <div class="search-history"></div>
+          <div class="search-history">
+
+          </div>
         </div>
       </scroll>
     </div>
     <div class="search-result" v-show="query">
-      <suggest :query="query"/>
+      <suggest :query="query" @listScroll="blurInput"/>
     </div>
     <router-view/>
   </div>
@@ -67,20 +69,25 @@
   .search
     .search-box-wrapper
       margin: 20px
+
     .shortcut-wrapper
       position: fixed
       top: 178px
       bottom: 0
       width: 100%
+
       .shortcut
         height: 100%
         overflow: hidden
+
         .hot-key
           margin: 0 20px 20px 20px
+
           .title
             margin-bottom: 20px
             font-size: $font-size-medium
             color: $color-text-l
+
           .item
             display: inline-block
             padding: 5px 10px
@@ -89,22 +96,28 @@
             background: $color-highlight-background
             font-size: $font-size-medium
             color: $color-text-d
+
         .search-history
           position: relative
           margin: 0 20px
+
           .title
             display: flex
             align-items: center
             height: 40px
             font-size: $font-size-medium
             color: $color-text-l
+
             .text
               flex: 1
+
             .clear
               extend-click()
+
               .icon-clear
                 font-size: $font-size-medium
                 color: $color-text-d
+
     .search-result
       position: fixed
       width: 100%
